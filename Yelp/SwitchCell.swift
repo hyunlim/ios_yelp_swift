9598dfa818691153d9cb79996eb7cbb0101500b9
Hyun Lim <hlim@lyft.com>
2016-02-18 15:44:08 -0800

    Filters state

--- Yelp/SwitchCell.swift
+++ Yelp/SwitchCell.swift
@@ -8,20 +8,34 @@
 
 import UIKit
 
+@objc protocol SwitchCellDelegate {
+    
+    optional func switchCell(switchCell:SwitchCell, didChangeValue value: Bool)
+    
+}
+
 class SwitchCell: UITableViewCell {
 
     @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var toggleSwitch: UISwitch!
     
+    weak var delegate:SwitchCellDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        self.toggleSwitch.addTarget(self, action: "switchValueChanged", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func switchValueChanged() {
+        delegate?.switchCell?(self, didChangeValue: self.toggleSwitch.on)
     }
 
 }
